b6f5313dd41b69835f54d23c36f5c2b442dc2123
Orion Davis <ord4@zips.uakron.edu>
2019-02-17 15:28:14 -0500

    Updated simulation to run through one cycle of PCR.

--- project1/pcr.py
+++ project1/pcr.py
@@ -1,30 +1,65 @@
-from random import choice
+from random import choice, randint
 
 class PCRSimulation():
     def __init__(self):
         self.dna = DNA()
-    
+
     def run(self, cycles=50):
-        # Denature
-        # Anneal
-        # Extend
-        pass
+        res = self.simulate(self.dna)
     
+    def simulate(self, dna):
+        # Denature
+        strands = self.__denature(dna)
+        
+        # Anneal
+        primed_strands = self.__anneal(strands)
+        
+        # Extend
+        matching_three_strand = self.__extension(primed_strands[0])
+        matching_five_strand = self.__extension(primed_strands[1])
 
-    def __denature(self):
-        pass
+        return [dna.combine_strands(strands[0], matching_three_strand), dna.combine_strands(strands[1], matching_five_strand)]
 
-    def __anneal(self):
-        pass
 
-    def __extension(self):
-        pass
+    def __denature(self, dna):
+        # Reversing three prime strand for simplicity in interaction
+        return [dna.five_prime, dna.three_prime[::-1]]
+
+    def __anneal(self, strands):
+        return [strands[0][20::], strands[1][20::]]
+
+    def __extension(self, strand, fall_off_const=200):
+        match = ""
+        for i in range(fall_off_const+randint(-50,50)):
+            match += self.dna.match_base_pair(strand[i])
+
+        return match
 
 class DNA():
-    def __init__(self, base_pairs=2000, amplify=200):
+    def __init__(self, five=None, three=None):
+        self.five_prime = five
+        self.three_prime = three
+        
+        if self.five_prime == None and self.three_prime == None:
+            self.generate_random_dna()
+    
+    def generate_random_dna(self, base_pairs=300, amplify=1):
         self.five_prime = self.__generate_dna_template(base_pairs, amplify)
         self.three_prime = self.__match_template(self.five_prime)
 
+    def match_base_pair(self, base):
+        if base == "A":
+            return "T"
+        elif base == "T":
+            return "A"
+        elif base == "G":
+            return "C"
+        elif base == "C":
+            return "G"
+
+    def combine_strands(self, five, three):
+        return DNA(five, three)
+        
     def __generate_dna_template(self, base_pairs, amplify):
         dna = ""
 
